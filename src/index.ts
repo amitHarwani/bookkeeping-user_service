b1ad259d0cc47f3a08cbd2c5c21c2fcb2df1b54a
amitHarwani <harwania21@gmail.com>
2024-08-03 19:40:38 +0400

    Init Commit

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ import dotenv from "dotenv"
 dotenv.config();
 dotenv.config({path: `.env.${process.env.NODE_ENV}`});
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, async () => {
     console.log(`User Service Listening On Port: ${process.env.PORT}`);
 })
